6f5584ff6b20655fab4a18525410583c2cbb2b86
Nikolay Petkov <nikolaytsenkovpetkov@gmail.com>
2025-03-04 12:19:03 +0200

    add username to chat

--- src/views/chat/userInfoSidebar/UserInfoSidebar.jsx
+++ src/views/chat/userInfoSidebar/UserInfoSidebar.jsx
@@ -2,26 +2,50 @@
 import "./UserInfoSidebar.css";
 
 //Dependency imports
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../../../context/AppContext";
 
 //Components imports
 import Button from "../../../components/button/Button";
+import { getUserData } from "../../../services/users.service";
 
 function UserInfoSidebar() {
-  const { onLogout } = useContext(AppContext);
+  const { onLogout, user } = useContext(AppContext);
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      getUserData(user.uid)
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const data = snapshot.val();
+            const username = Object.keys(data)[0]; // Extract the username
+            setUsername(username);
+          } else {
+            console.error("User data not found");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    }
+  }, [user]);
 
   const handleLogout = () => {
     onLogout();
     navigate("/login");
   };
 
+  if (!user) {
+    handleLogout();
+    return null;
+  }
+
   return (
     <div className="user-info-sidebar">
-      <h2>User Info</h2>
-      {/* Add your user info and previous chats here */}
+      <p className="username">Username: {username}</p>
       <Button
         label="Logout"
         onClick={handleLogout}
