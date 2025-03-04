8eb261864edc662eb860aabe6d17f71ab3cd7978
Nikolay Petkov <nikolaytsenkovpetkov@gmail.com>
2025-03-04 09:58:32 +0200

    remove react-hook-form

--- src/views/chat/channelsSidebar/ChannelsSidebar.jsx
+++ src/views/chat/channelsSidebar/ChannelsSidebar.jsx
@@ -1,8 +1,15 @@
+//misc imports
 import "./ChannelsSidebar.css";
+
+//Component imports
+import LogoWithText from "../../../components/logo/LogoWithText";
 
 function ChannelsSidebar() {
   return (
     <div className="sidebar">
+      <div className="logo-container">
+        <LogoWithText />
+      </div>
       <h2>Channels</h2>
       {/* Add your channels list here */}
     </div>
