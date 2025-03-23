a9656f11908bed67326aa39118d3a1e3aedaf7f1
Deshev1 <stefandeshev@gmail.com>
2025-03-23 17:32:29 +0200

    conditionally render messges (first by user or subsequent) and added styling for discordeque messages

--- src/views/Dashboard/ChatWindow/ChatBody/ChatBody.jsx
+++ src/views/Dashboard/ChatWindow/ChatBody/ChatBody.jsx
@@ -3,22 +3,27 @@ import "./ChatBody.css";
 
 import { useEffect, useState, useContext, useRef } from "react";
 
+import Avatar from "../../../../components/avatar/Avatar";
+
 import {
   sendMessage,
   updateChatMessages,
 } from "../../../../services/chat.service";
 import { AppContext } from "../../../../context/AppContext";
 
-function ChatBody({ chatData, setChatData }) {
+function ChatBody({ chatData, setChatData, receiversData }) {
   const { user, userData } = useContext(AppContext);
   const [currentMessage, setCurrentMessage] = useState("");
   const [messages, setMessages] = useState(null);
   const messagesEndRef = useRef(null);
+  let lastSender = null;
 
+  //Add on value listener
   useEffect(() => {
     updateChatMessages(chatData.uid, setChatData);
   }, []);
 
+  //On chatData change get messages and sort them
   useEffect(() => {
     setMessages(
       chatData?.messages
@@ -29,6 +34,7 @@ function ChatBody({ chatData, setChatData }) {
     );
   }, [chatData]);
 
+  //On messages change scroll to bottom of messages
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
@@ -37,12 +43,48 @@ function ChatBody({ chatData, setChatData }) {
     <div className="chat-body">
       <div className="messages-container">
         {messages
-          ? Object.values(messages).map((message) => (
-              <div key={message[0]}>
-                <p>sender: {message[1].username}</p>
-                <p>{message[1].text}</p>
-              </div>
-            ))
+          ? Object.values(messages).map((message) => {
+              //Render messages differently depending on if the previous message was sent by the same user
+              //Render this if its a subsequent message
+              console.log(receiversData);
+              if (lastSender === message[1].sender)
+                return (
+                  <div
+                    className="subsequent-message-container"
+                    key={message[0]}
+                  >
+                    <p className="subsequent-message">
+                      <span className="subsequent-message-timestamp">
+                        {new Date(message[1].createdOn).toLocaleTimeString()}
+                      </span>
+                      {message[1].text}
+                    </p>
+                  </div>
+                );
+
+              lastSender = message[1].sender;
+
+              //Render this if its the first message
+              return (
+                <div
+                  className="first-message-container"
+                  key={message[0]}
+                >
+                  <div className="sender-avatar-container">
+                    <Avatar></Avatar>
+                  </div>
+                  <div>
+                    <p className="first-message">
+                      {message[1].username}
+                      <span className="first-message-timestamp">
+                        {new Date(message[1].createdOn).toLocaleString()}
+                      </span>
+                    </p>
+                    <p>{message[1].text}</p>
+                  </div>
+                </div>
+              );
+            })
           : ""}
         <div ref={messagesEndRef}></div>
       </div>
@@ -50,7 +92,7 @@ function ChatBody({ chatData, setChatData }) {
         className="chat-input-container"
         onSubmit={(е) => {
           е.preventDefault();
-          setCurrentMessage("");
+          if (currentMessage.trim() === "") return;
           sendMessage(
             {
               text: currentMessage,
@@ -60,6 +102,7 @@ function ChatBody({ chatData, setChatData }) {
             },
             chatData.uid
           );
+          setCurrentMessage("");
         }}
       >
         <input
